7932a416f566959f875851fcf4b3f31191582d94
Masayoshi Wada <developer@andantesoftware.com>
2017-06-05 23:37:35 +0900

    use meow instead of minimist

--- bin/format-yarn-outdated.js
+++ bin/format-yarn-outdated.js
@@ -2,9 +2,11 @@
 
 const yaml = require('js-yaml');
 const fs   = require('fs');
+const meow = require('meow');
 
-const argv      = require('minimist')(process.argv.slice(2));
 const Formatter = require('../lib/Formatter');
+
+const cli = meow();
 
 const selectFormat = val => ['markdown', 'json', 'mackerel'].includes(val) ? val : 'markdown';
 const loadYAML = val => val ? yaml.safeLoad(fs.readFileSync(val, 'utf8')) : null;
@@ -16,9 +18,9 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (c) => { stdin += c });
 process.stdin.on('end', () => {
   const json       = JSON.parse(stdin);
-  const format     = selectFormat(argv.format);
-  const excludes   = loadYAML(argv.excludes) || [];
-  const changelogs = loadYAML(argv.changelogs) || {};
+  const format     = selectFormat(cli.flags.format);
+  const excludes   = loadYAML(cli.flags.excludes) || [];
+  const changelogs = loadYAML(cli.flags.changelogs) || {};
 
   const formatter = new Formatter(format, excludes, changelogs);
   console.log(formatter.run(json));
