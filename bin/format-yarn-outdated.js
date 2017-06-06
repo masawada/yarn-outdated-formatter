1531542269ddb16ac6233d52b60e8eaea2fd0254
pastak <pasta0915@gmail.com>
2017-06-06 21:22:33 +0900

    add `--generate-yaml`
    Preparing `changelogs.yaml` is too hard.
    
    This will support `changelog.yaml`. If there is CHNAGELOG file on
    `node_modules/MODULE_NAME`'s 1st level, it outputs its CHANGELOG URL.

--- bin/format-yarn-outdated.js
+++ bin/format-yarn-outdated.js
@@ -5,6 +5,7 @@ const fs   = require('fs');
 const meow = require('meow');
 
 const Formatter = require('../lib/Formatter');
+const generateYaml = require('../lib/generateYaml')
 
 const cli = meow(`
   NAME
@@ -19,6 +20,7 @@ const cli = meow(`
     --version, -v    Prints the package version.
     --format, -f     Output format. One of either markdown, json or mackerel can be used. Default: markdown
     --excludes, -e   Path to YAML file which specify package names to exclude
+    --generate-yaml  Generate YAML file which specify changelog uris for the packages
     --changelogs, -c Path to YAML file which specify changelog uris for the packages
 
   EXAMPLES
@@ -38,6 +40,11 @@ const cli = meow(`
 
 const selectFormat = val => ['markdown', 'json', 'mackerel'].includes(val) ? val : 'markdown';
 const loadYAML = val => val ? yaml.safeLoad(fs.readFileSync(val, 'utf8')) : null;
+
+if (cli.flags.generateYaml) {
+  generateYaml();
+  process.exit(0);
+}
 
 // observe stdin
 let stdin = '';
